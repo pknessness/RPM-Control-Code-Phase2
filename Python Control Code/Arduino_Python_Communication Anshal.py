81f37873bbf580a882eee0c3427eac69e6f90fe4
Andrew Copeland <apcopeland20@gmail.com>
2024-06-08 14:28:22 -0700

    it literally works

--- Python Control Code/Arduino_Python_Communication Anshal.py
+++ Python Control Code/Arduino_Python_Communication Anshal.py
@@ -5,6 +5,7 @@ import numpy
 
 # Configure the serial connection
 ser = serial.Serial('COM6', 115200)
+f = open("demofile2.txt", "a")
 
 # Initialize array storing accel vectors
 accel_array = []
@@ -17,28 +18,33 @@ def send_command(command):
         #response = ser.readline().decode().strip() #ser.readline takes one line of text from the serial port, .decode turns from bytes to string (UTF-8), .strip removes any white spaces from the string
         #print(f"Arduino: {response}") #Only include if variable in arduino sends data back
 def wait_for_ready(ser):
+    global f
     while True:
         if ser.in_waiting > 0:
             response = ser.readline().decode().strip()
-            print(response)
-            if "READY" in response:
+            
+            if "--" in response:
                 break
             else:
+                print(response)
+                f.write(response + "\n")
                 accel_array.append(response)
         else:
             time.sleep(0.01)  # Short delay to reduce CPU usage
 
 
-with open('MOTORINPUTS_64_10000.csv', newline='') as csvfile: #create the points row by row
+with open('MOTORINPUTS_64_2000.csv', newline='') as csvfile: #create the points row by row
     read = csv.reader(csvfile) #csv.reader reads row by row
     time.sleep(5)
     for row in read:
         inputA = row[0]
         inputB = row[1]
-        print(inputA + "," + inputB + ";")
-        send_command(inputA + "," + inputB + ";")
         wait_for_ready(ser)
+
+        #print(inputA + "," + inputB + ";")
+        send_command(inputA + "," + inputB + ";")
+        
         # print(inputB)
         # send_command(inputB) #+";" to add semicolon to end of row
         # wait_for_ready(ser)
-    numpy.savetxt("accel_vec.csv",accel_array,delimiter=',')
+    numpy.savetxt("accel_vec.csv",accel_array,delimiter='\t')
