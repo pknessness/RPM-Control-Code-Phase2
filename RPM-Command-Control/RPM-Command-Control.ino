fbb4a8598775592d82fb51e56427e4e471eee776
Anshal Jain <pk.ness.ness@gmail.com>
2024-07-28 02:53:44 -0700

    it works fairly smoothly, the algorithm works but params might need tweaking

--- RPM-Command-Control/RPM-Command-Control.ino
+++ RPM-Command-Control/RPM-Command-Control.ino
@@ -8,24 +8,24 @@
   MIT License
   https://github.com/codeljo/AA_MCP2515
 */
-#define MAX_VELO_RPM 10
+#define MAX_VELO_RPM 8
 #define ACCEL_RAD_S_S 0.2
-#define DT_MS 300
-#define ANGLE_OF_ATTACK 15
+#define DT_MS 1500
+#define ANGLE_OF_ATTACK 10
 #define SEED 2132138
 
 #include "AA_MCP2515.h"
 
 struct Motor {
-  uint16_t angle;
-  uint16_t velocity;
-  uint16_t current;
+  int16_t angle;
+  int16_t velocity;
+  int16_t current;
   uint8_t temp;
 };
 
 void printMotor(Motor m, char c = '?');
-Motor setVelocity(uint16_t canID, int32_t velocity_dps_hundreth);
-Motor setAngle(uint16_t canID, int16_t velocity_dps, int16_t angle_deg_hundreth);
+void setVelocity(uint16_t canID, int32_t velocity_dps_hundreth);
+void setAngle(uint16_t canID, int16_t velocity_dps, int16_t angle_deg_hundreth);
 
 // TODO: modify CAN_BITRATE, CAN_PIN_CS(Chip Select) pin, and CAN_PIN_INT(Interrupt) pin as required.
 const CANBitrate::Config CAN_BITRATE = CANBitrate::Config_8MHz_500kbps;
@@ -49,6 +49,8 @@ uint8_t reverseB = 0;
 Motor motorA;
 Motor motorB;
 
+Motor motors[2] = {motorA, motorB};
+
 double heading = 0;
 
 String inst;
@@ -68,49 +70,74 @@ void setup() {
   }
   Serial.println("CAN begin OK");
 
+  setCurrent(0x141,0);
+  setCurrent(0x142,0);
+
+
   randomSeed(SEED);
 }
 
 void loop() {
 
-  if(Serial.available()){
-    char inChar = Serial.read();
-    // Serial.println("--");
-    // Serial.println(inChar);
-    // Serial.println((int)inChar);
-    // Serial.println("--");
-    if(inChar >= 48 && inChar <= 57){
-      inst += inChar;
-    }else if(inChar == 'q'){
-      reverseA = !reverseA;
-    }else if(inChar == 'e'){
-      reverseB = !reverseB;
-    }else if(inChar == endCharA){
-      angleA = atoi(inst.c_str());
+  // if(Serial.available()){
+  //   char inChar = Serial.read();
+  //   // Serial.println("--");
+  //   // Serial.println(inChar);
+  //   // Serial.println((int)inChar);
+  //   // Serial.println("--");
+  //   if(inChar >= 48 && inChar <= 57){
+  //     inst += inChar;
+  //   }else if(inChar == 'q'){
+  //     reverseA = !reverseA;
+  //   }else if(inChar == 'e'){
+  //     reverseB = !reverseB;
+  //   }else if(inChar == endCharA){
+  //     angleA = atoi(inst.c_str());
 
-      Serial.print("new angle: ");
-      Serial.println(angleA);
-      setVelocity(0x141, angleA);
+  //     Serial.print("new angle: ");
+  //     Serial.println(angleA);
+  //     setVelocity(0x141, angleA);
 
-      inst = "";
-    }else if(inChar == endCharB){
-      angleB = atoi(inst.c_str());
-      // Serial.print("new angleB: ");
-      // Serial.println(angleB);
+  //     inst = "";
+  //   }else if(inChar == endCharB){
+  //     angleB = atoi(inst.c_str());
+  //     // Serial.print("new angleB: ");
+  //     // Serial.println(angleB);
 
-      inst = "";
-    }else if(inChar == purge){
-      inst = "";
-    }
+  //     inst = "";
+  //   }else if(inChar == purge){
+  //     inst = "";
+  //   }
     
-  }
+  // }
 
-  if(innerLoopCount > 200){
-    
+
+  if(innerLoopCount > DT_MS){
+    heading += (random(65536)/65536.0) * ANGLE_OF_ATTACK + (ANGLE_OF_ATTACK/2);
+    double heading_rad = heading * 3.14159 / 180;
+    setVelocity(0x141,(int32_t)(sin(heading_rad)*MAX_VELO_RPM * 6 * 100));
+    setVelocity(0x142,(int32_t)(cos(heading_rad)*MAX_VELO_RPM * 6 * 100));
+    // printMotor(motorA,'a');
+    // printMotor(motorB,'b');
     innerLoopCount = 0;
+  }else{
+    queryMotor(0x141);
+    queryMotor(0x142);
   }
   innerLoopCount ++;
-  
+
+  getFeedback();
+
+  Serial.print("Heading:");
+  Serial.print(heading);
+  Serial.print(" A Des:[");
+  Serial.print(sin(heading)*MAX_VELO_RPM);
+  Serial.print("] Act:[");
+  Serial.print(motors[0].velocity/100.0);
+  Serial.print("] B Des:");
+  Serial.print(cos(heading)*MAX_VELO_RPM);
+  Serial.print("] Act:[");
+  Serial.println(motors[1].velocity/100.0);
 
   delay(1);
 }
@@ -130,39 +157,87 @@ void printMotor(Motor m, char c = '?'){
   Serial.println(m.temp);
 }
 
-Motor setVelocity(uint16_t canID, int32_t velocity_dps_hundreth){
-    // uint8_t data[8] = { 
-    //     0xA2, 
-    //     0x00, 
-    //     0x00, 
-    //     0x00, 
-    //     (uint8_t)(velocity_dps_hundreth), 
-    //     (uint8_t)(velocity_dps_hundreth>>8), 
-    //     (uint8_t)(velocity_dps_hundreth>>16), 
-    //     (uint8_t)(velocity_dps_hundreth>>24)};
+void setVelocity(uint16_t canID, int32_t velocity_dps_hundreth){
+    uint8_t data[8] = { 
+        0xA2, 
+        0x00, 
+        0x00, 
+        0x00, 
+        (uint8_t)(velocity_dps_hundreth), 
+        (uint8_t)(velocity_dps_hundreth>>8), 
+        (uint8_t)(velocity_dps_hundreth>>16), 
+        (uint8_t)(velocity_dps_hundreth>>24)};
 
-    uint8_t data[8] = {0xA2, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00};
+    // uint8_t data[8] = {0xA2, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00};
+    CANFrame frame(canID, data, sizeof(data));
+    // frame.print("TX");
+    CAN.write(frame);
+}
 
-    Motor mot;
+void setAngleSingle(uint16_t canID, int16_t velocity_dps, int16_t angle_deg_hundreth){
+    uint8_t data[8] = { 
+        0xA6, 
+        0x00, 
+        (uint8_t)(velocity_dps), 
+        (uint8_t)(velocity_dps>>8), 
+        (uint8_t)(angle_deg_hundreth), 
+        (uint8_t)(angle_deg_hundreth>>8), 
+        0x00, 
+        0x00};
+
+    // uint8_t data[8] = {0xA2, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00};
     CANFrame frame(canID, data, sizeof(data));
     frame.print("TX");
     CAN.write(frame);
-
-    if (CAN.read(frame) == CANController::IOResult::OK) {
-        frame.getData(data, 8);
-        if(data[0] == 0xA2){
-            mot.angle = data[6] | (data[7]<<8); 
-            mot.velocity = data[4] | (data[5]<<8);
-            mot.current = data[2] | (data[3]<<8);
-            mot.temp = data[1];
-        }else{
-            frame.print("WUT");
-        }
-    }
-    return mot;
 }
 
-Motor setAngle(uint16_t canID, int16_t velocity_dps, int16_t angle_deg_hundreth){
-    Motor mot;
-    return mot;
+void setCurrent(uint16_t canID, int16_t current_hundreth){
+    uint8_t data[8] = { 
+        0xA1, 
+        0x00, 
+        0x00, 
+        0x00, 
+        (uint8_t)(current_hundreth), 
+        (uint8_t)(current_hundreth>>8), 
+        0x00, 
+        0x00};
+
+    // uint8_t data[8] = {0xA2, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00};
+    CANFrame frame(canID, data, sizeof(data));
+    frame.print("TX");
+    CAN.write(frame);
+}
+
+void queryMotor(uint8_t canID){
+  uint8_t data[8] = { 
+      0x9C, 
+      0x00, 
+      0x00, 
+      0x00, 
+      0x00, 
+      0x00, 
+      0x00, 
+      0x00};
+
+  // uint8_t data[8] = {0xA2, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00};
+  CANFrame frame(canID, data, sizeof(data));
+  // frame.print("TX");
+  CAN.write(frame);
+}
+
+void getFeedback(){
+  uint8_t data[8] = {0,0,0,0,0,0,0,0};
+  CANFrame frame(0x140,data,8);
+  while (CAN.read(frame) == CANController::IOResult::OK) {
+      frame.getData(data, 8);
+      uint16_t id = frame.getId()-0x241;
+      if(data[0] == 0xA1 || data[0] == 0xA2 || data[0] == 0xA6 || data[0] == 0x9C){
+          motors[id].angle = (int16_t)(data[6] | (data[7]<<8)); 
+          motors[id].velocity = (int16_t)(data[4] | (data[5]<<8));
+          motors[id].current = (int16_t)(data[2] | (data[3]<<8));
+          motors[id].temp = data[1];
+      }else{
+          frame.print("WUT");
+      }
+  }
 }
